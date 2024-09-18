0f3f44a4cb95568f5ef262d71b8c3612de501a48
kevinziroldi <k.ziroldi@gmail.com>
2024-09-18 15:34:15 +0200

    changes to GuessTheFlag (day 23)

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -13,6 +13,13 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var tappedFlag = ""
+    
+    @State private var showingGameEnded = false
+    @State private var numberTapped = 0
+    @State private var numberCorrect = 0
+    
+    private let maxGames = 10
     
     var body: some View {
         ZStack {
@@ -20,6 +27,10 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack(spacing: 30) {
+                Text("Played flags: \(numberTapped)/\(maxGames)")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle.weight(.semibold))
+                
                 VStack {
                     Text("Tap the flag of").foregroundStyle(.white)
                         .font(.subheadline.weight(.heavy))
@@ -36,28 +47,55 @@ struct ContentView: View {
                             .shadow(radius: 5)
                     }
                 }
+                
+                Text("Score: " + scoreTitle)
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
             }
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            if scoreTitle == "Correct" {
+                Text("Correct!")
+            }else {
+                Text("Wrong! That’s the flag of " + tappedFlag)
+            }
+        }
+        .alert("Game ended!", isPresented: $showingGameEnded) {
+            Button("Restart game", action: restartGame)
+        } message: {
+            Text("You scored \(numberCorrect)/\(maxGames) correct answers")
         }
     }
     
     func flagTapped(_ number: Int) {
+        numberTapped += 1
+        
         if number == correctAnswer {
             scoreTitle = "Correct"
+            numberCorrect += 1
         } else {
             scoreTitle = "Wrong"
         }
-
+        
+        tappedFlag = countries[number]
         showingScore = true
     }
 
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        if(numberTapped == maxGames) {
+            showingGameEnded = true
+        }else {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
+    }
+    
+    func restartGame() {
+        numberTapped = 0
+        numberCorrect = 0
+        askQuestion()
     }
 }
 
